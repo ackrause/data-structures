148aefa8c364196c4582f2ba980d87c75ab3b150
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-16 16:04:04 -0700

    Implemented a queue using the functional-shared pattern. Similar workflow as functional-shared stack.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,35 @@
-var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+var extend = function(patient, surgeon){
+  for (var key in surgeon){
+    patient[key] = surgeon[key];
+  }
 };
 
-var queueMethods = {};
+
+var queueMethods = {
+  enqueue : function(value){
+    this.storage[this.end] = value;
+    this.end++;
+  },
+
+  dequeue : function(){
+    var temp = this.storage[this.start];
+    delete this.storage[this.start];
+    this.start++;
+    if (this.start > this.end){this.start = this.end = 0;}
+    return temp;
+  },
+
+  size : function(){
+    return this.end - this.start;
+  },
+
+};
+
+var makeQueue = function(){
+  var instance = {};
+
+  extend(instance, {storage: {}, start: 0, end: 0});
+  extend(instance, queueMethods);
+
+  return instance;
+};
