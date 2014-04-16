b82b5b947741b6b4c2fae2d454e5fb4a130accd6
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-16 16:17:29 -0700

    Implemented stacks with a prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,28 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var stackMethods = {
+  push : function(value){
+    this.storage[this._size] = value;
+    this._size++;
+  },
+
+  pop : function(){
+    if(this._size !== 0){
+      this._size--;
+    }
+    var temp = this.storage[this._size];
+    delete this.storage[this._size];
+    return temp;
+  },
+
+  size : function(){
+    return this._size;
+  }
 };
 
-var stackMethods = {};
+var makeStack = function() {
+  var instance = Object.create(stackMethods);
+
+  instance.storage = {};
+  instance._size = 0;
+
+  return instance;
+};
