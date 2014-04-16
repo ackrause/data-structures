a041d64d0d97bb2a7d87aad909833cd076e38d98
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-16 15:52:47 -0700

    Functional-shared pattern of stack. Rewritten so maker function extends returned value with properties defined elsewhere.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,38 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+var extend = function(objToExtend, extension){
+  for(var key in extension){
+    objToExtend[key] = extension[key];
+  }
 };
-  
-var stackMethods = {};
+
+
+var stackMethods = {
+  push : function(value){
+    this.storage[this._size] = value;
+    this._size++;
+  },
+
+  pop : function(){
+    if(this._size !== 0){
+      this._size--;
+    }
+    var temp = this.storage[this._size];
+    delete this.storage[this._size];
+    return temp;
+  },
+
+  size : function(){
+    return this._size;
+  }
+};
+
+var makeStack = function() {
+  var instance = {};
+
+  instance.storage = {};
+  instance._size = 0;
+
+  extend(instance, stackMethods);
+
+  return instance;
+};
+
