af6a4750b0e5040ee95d8c8afa0ea92e2c422d17
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-16 16:43:42 -0700

    Implemented queue using a pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,22 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(){
+  this.storage = {};
+  this.start = 0;
+  this.end = 0;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.end] = value;
+  this.end++;
+};
+
+Queue.prototype.dequeue = function(){
+  var temp = this.storage[this.start];
+  delete this.storage[this.start];
+  this.start++;
+  if (this.start > this.end){this.start = this.end = 0;}
+  return temp;
+};
+
+Queue.prototype.size = function(){
+  return this.end - this.start;
 };
