1a7c49fd419c11d8522680e4a0db639db9ca43d4
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-17 11:04:15 -0700

    Implemented contains() for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -10,7 +10,15 @@ setPrototype.add = function(item){
 };
 
 setPrototype.contains = function(item){
+  if (this._storage[item]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item){
 };
+
+// var mySet = makeSet();
+// mySet.add('hi');
