110d5869c6a35f997111ef9c34ea22eebc68873f
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-17 10:34:33 -0700

    Implemented addChild to tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,14 @@
+var extend = function(objToExtend, extension) {
+  for (var key in extension) {
+    objToExtend[key] = extension[key];
+  }
+};
+
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -9,8 +16,14 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var child = makeTree(value);
+  if (this.children === undefined) {
+    this.children = [];
+  }
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target){
+
 };
 
