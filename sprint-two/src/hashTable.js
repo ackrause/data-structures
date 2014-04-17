ba907e13e1457bfaf9b2a3e8679cf7d1b2d911c4
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-17 11:19:10 -0700

    Implemented insert() for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,7 +5,7 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  this._storage.set(i, v);
 };
 
 HashTable.prototype.retrieve = function(k){
