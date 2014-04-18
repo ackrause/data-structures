5ad3b7d9aa12983c6e434cf8d9b0097b2c3e3b21
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-18 15:47:32 -0700

    Can add nodes to graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,10 +1,14 @@
 var Graph = function(){
+  this.nodes = {};
 };
 
-Graph.prototype.addNode = function(newNode, toNode){
+Graph.prototype.addNode = function(value){
+  var newNode = {value: value, edges: {}};
+  this.nodes[value] = newNode;
 };
 
 Graph.prototype.contains = function(node){
+  return this.nodes.hasOwnProperty(node) ? true : false;
 };
 
 Graph.prototype.removeNode = function(node){
