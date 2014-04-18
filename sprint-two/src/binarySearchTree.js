f3e6fdd7577e77a57995071b573371e6d030101f
Hack Reactor Students <a.c.krause@gmail.com>
2014-04-18 15:06:36 -0700

    Can insert() into the bst

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,28 @@
 var makeBinarySearchTree = function(value){
+  var instance = Object.create(binarySearchTreeMethods);
+  instance.left = null;
+  instance.right = null;
+  instance.value = value;
+  return instance;
+};
+
+var binarySearchTreeMethods = {
+  insert : function(value){
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = makeBinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (!this.right) {
+        this.right = makeBinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  },
+
+  contains : function(){},
+  depthFirstLog : function(){},
 };
